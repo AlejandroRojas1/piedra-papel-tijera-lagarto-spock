4f93e92cf8909ebb39a3c38cde407596a011a64f
Alejandro Rojas <alejandrofabianrojas64@gmail.com>
2025-12-12 06:02:05 -0300

    initial styles of rules card

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import PiedraPapelTijera from './piedraPapelTijera'
+import  RulesCard  from './components/RulesCard'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <PiedraPapelTijera />
+    <RulesCard />
   </StrictMode>,
 )
