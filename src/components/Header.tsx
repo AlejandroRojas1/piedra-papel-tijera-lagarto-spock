2f0a6126c3c77a27fafeea0a4a30f91cd4e5a88a
Alejandro Rojas <alejandrofabianrojas64@gmail.com>
2025-12-13 20:24:42 -0300

    added and stylized Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,24 @@
+
+const score: number = 12;
+
+const Header = () => {
+  return (
+    <>
+        <div className="w-[50%] flex border-solid rounded-xl border-(--Gray400) border-2 mx-auto mt-20 p-10 justify-between items-center">
+            <div className="flex flex-col justify-center">
+                <span className="font-bold text-white py-0 my-0 leading-none">ROCK</span> 
+                <span className="font-bold text-white py-0 my-0 leading-none">PAPER</span>
+                <span className="font-bold text-white py-0 my-0 leading-none">SCISSORS</span>
+                <span className="font-bold text-white py-0 my-0 leading-none">LIZZARD</span>
+                <span className="font-bold text-white py-0 my-0 leading-none">SPOCK</span>
+            </div>
+            <div className="flex flex-col bg-white rounded w-[6rem] h-[5rem] justify-center items-center">
+                <span className="text-(--Blue700) tracking-widest text-xs font-bold">SCORE</span>
+                <span className="text-5xl text-(--Gray600) font-bold tracking-tighter">{score}</span>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Header
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import  RulesCard  from './components/RulesCard'
 import './index.css'
+import Game from './components/Game'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RulesCard />
+    <Game />
   </StrictMode>,
 )
